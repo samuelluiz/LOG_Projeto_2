0e67cdb89b11a620f4456e348094b8482427df6f
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2018-05-09 15:11:24 -0300

    Update Q1.thy

--- Q1.thy
+++ Q1.thy
@@ -1,15 +1,15 @@
-  theory Q1
+  theory Q2
     imports
 begin 
-theorem AB_PROVA_BA: "(A^B) ⟶ (B^A)"
+theorem : "(A∧B) ⟶ (B∧A)"
 proof ―
-  have ab:"(A^B) ⟹ (B^A) "
+  have ab:"(A∧B) ⟹ (B∧A) "
   proof ―
-    assume ab: "A^B"
+    assume ab: "A∧B"
     from ab have a: "A" by (rule conjE)
     from ab have b: "B" by (rule conjE)
-    from b and a have ba: "B^A" by (rule conjI)
+    from b and a have ba: "B∧A" by (rule conjI)
   qed
-  from ab show "(A^B)⟶(B^A)" by (rule impI)
+  from ab show "(A∧B)⟶(B∧A)" by (rule impI)
 qed
 end
