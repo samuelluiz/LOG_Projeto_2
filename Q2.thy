cb72e669a0c7778749f9461bcb79e692a9ae61f8
Samuel Luiz <30871470+samuelluiz@users.noreply.github.com>
2018-05-11 11:21:26 -0300

    Update Q2.thy

--- Q2.thy
+++ Q2.thy
@@ -10,26 +10,29 @@ proof -
    have rp: "R⟹P"
    proof -
      assume r: "R"
-     from rpq and r show pq: "P∨Q" by (rule impE) 
-     from rfalseq and r show falseq: "¬Q" by (rule impE)
+     from rpq and r have  pq: "P∨Q" by (rule impE) 
+     from rfalseq and r have nq: "¬Q" by (rule impE)
 
      have npfalse: "¬P ⟹ False"
      proof -
        assume np: "¬P"
+       
        have falseq: "Q ⟹ False"
        proof -
          assume q: "Q"
-         from falseq and q show "False" by (rule notE)
+         from nq and q show "False" by (rule notE)
        qed
+      
        have pfalse: "P ⟹ False"
        proof - 
          assume p: "P"
          from np and p show "False" by (rule notE)
        qed
-       from pq and falseq and pfalse show "False" by (rule disjE)
+       
+       from pq and pfalse and falseq show "False" by (rule disjE)
      qed
      from npfalse have nnp: "¬¬P" by (rule notI)
-     from nnp have p: "P" by (rule notnotD)
+     from nnp show p: "P" by (rule notnotD)
    qed
    from rp show "R ⟶ P" by (rule impI)
  qed
